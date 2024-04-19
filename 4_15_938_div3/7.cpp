f8e79df5169f9380c09c7914e0b6d4ff22c808cd
qi7zai <142134303+qi7zai@users.noreply.github.com>
2024-04-19 21:58:59 +0800

    Update 7.cpp

--- 4_15_938_div3/7.cpp
+++ 4_15_938_div3/7.cpp
@@ -1,55 +1,41 @@
-#include <bits/stdc++.h>
+//暴力？
+//遍历a[0][0]和a[n-1][m-1]的所有公因数，判断是否能有某条路径上的所有数都含有这个因子
+//f数组定义在循环体内会！超！时！！！
+#include<bits/stdc++.h>
 
 using namespace std;
-
 typedef long long ll;
-vector<vector<ll>> a(102,vector<ll >(102,0));
-int n,m;
-ll ans;
-
-ll gcd(ll a,ll b)
-{
-    if(a < b)swap(a,b);
-    if(!a || !b)return max(a,b);
-    else
-    {
-        return gcd(b,a % b);
-    }
-}
-void dfs(int i,int j,int now_value)
-{
-    if(i > n || j > m )return ;
-    ll now_gcd = gcd(a[i][j],now_value);
-    if(ans >= now_gcd)return ;
-    if(i == n && j == m)
-    {
-        ans = max(ans,now_gcd);
-        return;
-    }
-    else
-    {
-        dfs(i+1,j,now_gcd);
-        dfs(i,j+1,now_gcd);
-    }
-    return ;
-}
 
 void solve()
 {
-    cin>>n>>m;
-    ans = 0;
-    a.clear();
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n + 1, vector<int>(m + 1));
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            cin >> a[i][j];
 
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=m;j++)
-        {
-            cin>>a[i][j];
+    int max_gcd = gcd(a[1][1], a[n][m]);
+    int ans = 1;
+    vector<vector<int>> f(n + 1, vector<int>(m + 1, 0));    //该因子是否能从左上角到i,j处
+    //for(int num = max_gcd; num; num--) {  //这样遍历会超时
+    for(int num = 1; num * num <= max_gcd; num++) {
+        if(max_gcd % num) continue; //不是公因数
+        vector<int> nums = {num, max_gcd / num};
+        for(auto tmp : nums) {
+            for(int i = 0; i <= n; i++) f[i].resize()
+            f[1][0] = 1;
+            for(int i = 1; i <= n; i++)
+                for(int j = 1; j <= m; j++) {
+                    if(a[i][j] % tmp) f[i][j] = 0; //不含该因数
+                    else f[i][j] = f[i - 1][j] | f[i][j - 1];    //从上或左过来
+                }
+
+            if(f[n][m]) ans = max(ans, tmp);    //可达
         }
     }
-    dfs(1,1,0);
-    cout<<ans<<endl;
 
+    cout << ans << endl;
 }
 
 int main()
@@ -65,6 +51,7 @@ int main()
     {
         solve();
     }
+    
 
     return 0;
 }
