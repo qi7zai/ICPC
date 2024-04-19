b2f89c82e94e534bd39416a4aa024d71409627ae
qi7zai <142134303+qi7zai@users.noreply.github.com>
2024-04-19 11:24:33 +0800

    Update D.cpp

--- 4_18_164_div2/D.cpp
+++ 4_18_164_div2/D.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -8,6 +7,7 @@ void solve()
 {
     int n;
     cin >> n;
+
     ll mod = 998244353;
     vector<int> a(n);
     ll sum = 0;
@@ -16,22 +16,22 @@ void solve()
         sum += a[i];
     }
     sort(a.begin(), a.end());
-    vector<ll> f(sum, 0);
-    f[a[0]] = a[0];
-    for(int i = 1; i < n; i++) {
-        for(int j = sum - a[i]; j; j--) {
-            if(!f[j]) continue;
-            if(a[i] >= j) {
-                f[a[i] + j] = (f[j] + a[i] + j) % mod;
-            }
-            else {
-                f[a[i] + j] = (f[j] + a[i] + (j - a[i]) / 2 + (j - a[i]) & 1) % mod;
-            }
-        }
-    }
+
+    vector<ll> f(sum + 1, 0);
+    //f[i][j]表示前i元素中选出的颜色的气球数之和为j时，其组合的个数。由第i-1维得i维，则第一维可省略
+    //如{1, 1, 2} 则f[2] = 2，组合为{1, 1}或{2}
+    f[0] = 1;
     ll ans = 0;
-    for(int i = 0; i <= sum; i++) ans = (ans + f[i]) % mod;
-    cout << f[sum] << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = sum - a[i]; ~j; j--) {   //正反序都行 计算新增一种气球后带来的总value增加
+            if(!f[j]) continue;
+            if(a[i] >= j) ans = (ans + a[i] * f[j]) % mod;
+            else ans = (ans + (a[i] + (j - a[i]) / 2 + ((j - a[i]) & 1)) * f[j]) % mod;
+        }
+        for(int j = sum; j >= a[i]; j--)    //同01背包，避免影响
+            if(f[j - a[i]]) f[j] = (f[j] + f[j - a[i]]) % mod;  //更新f
+    }
+    cout << ans << endl;
 }
 
 int main()
